12d7bcfb253df84e7f090366b478225780c899a2
Volkan Özçelik <ovolkan@vmware.com>
2023-10-21 18:21:18 -0700

    add inspector to demo setup toi test the secret dispatch
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

--- examples/federation-workshop/cluster-1/hack/create-bundle-config-map.sh
+++ examples/federation-workshop/cluster-1/hack/create-bundle-config-map.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
 microk8s kubectl create configmap trust-bundle \
   --from-file=./vsecm-002-bundle.json -n spire-system
--- examples/federation-workshop/cluster-1/hack/print-bundle.sh
+++ examples/federation-workshop/cluster-1/hack/print-bundle.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
 SPIRE_SERVER=$(microk8s kubectl get po -n spire-system \
   | grep "spire-server-" | awk '{print $1}')
 
