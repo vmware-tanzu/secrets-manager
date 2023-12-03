01e97ba6cb74044b03cf458d54a2fd59858fbe5c
Volkan Özçelik <ovolkan@vmware.com>
2023-12-03 00:09:36 -0800

    documentation cache and some helper scripts (#340)
    * intermediate
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * ignore
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * add 0.21.4
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * added archived version of docs
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * quote change
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * add a snapshot version of the documentation
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * cached version of docs and some utility scripts
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * changes
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    ---------
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ hack/serve-docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+VERSION="0.21.4"
+DOCS_FOLDER="./docs/$VERSION"
+
+cd "$DOCS_FOLDER" || exit
+
+bundle exec jekyll serve
