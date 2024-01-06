97b01b1f16b8195869e49acfe0d8c315e6ac4321
Volkan Özçelik <ovolkan@vmware.com>
2024-01-05 22:54:58 -0800

    godoc
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

--- core/env/secret.go
+++ core/env/secret.go
@@ -14,6 +14,13 @@ import (
 	"os"
 )
 
+// SecretGenerationPrefix returns a prefix that’s used by VSecM Sentinel to
+// generate random pattern-based secrets. If a secret is prefixed with this value,
+// then VSecM sentinel will consider it as a “template” rather than a literal value.
+//
+// It retrieves this prefix from the environment variable
+// "VSECM_SAFE_SECRET_GENERATION_PREFIX".
+// If the environment variable is not set or is empty, it defaults to "gen:".
 func SecretGenerationPrefix() string {
 	p := os.Getenv("VSECM_SAFE_SECRET_GENERATION_PREFIX")
 	if p == "" {
