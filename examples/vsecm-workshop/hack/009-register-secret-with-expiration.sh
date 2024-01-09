be2459c59150f8e0f211d4e6d7e84fc857449012
Volkan Özçelik <ovolkan@vmware.com>
2024-01-09 14:04:57 -0800

    🐛 fix(VSecM): register secret with expiration
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ examples/vsecm-workshop/hack/009-register-secret-with-expiration.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+. ./env.sh
+
+kubectl exec "$SENTINEL" -n vsecm-system -- safe \
+  -w "example" \
+  -n "default" \
+  -s "VSecMRocks!" \
+  -N "2019-10-12T07:20:50.52Z" \
+  -E "2042-10-12T08:21:51.53Z"
