0a58fab2d29bfb9a6a51a610618ec384b3abdf74
Murat mirgun Ercan <muratmirgunercan225@gmail.com>
2024-01-12 19:45:43 +0300

    test: add test cases for crypto/value file
    Signed-off-by: Murat mirgun Ercan <muratmirgunercan225@gmail.com>

---
+++ core/crypto/parser_test.go
@@ -0,0 +1,118 @@
+package crypto
+
+import (
+	"testing"
+)
+
+func Test_rangesAndLength(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		length  int
+		wantErr bool
+	}{
+		{
+			name: "Success with Integer length",
+			args: args{
+				"example-input{3}",
+			},
+			want:    "example-input",
+			length:  3,
+			wantErr: false,
+		},
+		{
+			name: "Fail with Index out of range",
+			args: args{
+				"example-input{3",
+			},
+			want:    "example-input",
+			length:  0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := rangesAndLength(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("rangesAndLength() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("rangesAndLength() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.length {
+				t.Errorf("rangesAndLength() got1 = %v, want %v", got1, tt.length)
+			}
+		})
+	}
+}
+
+func Test_parseLength(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "Success with Integer length",
+			args: args{
+				"example-input{3}",
+			},
+			want:    3,
+			wantErr: false,
+		},
+		{
+			name: "Fail with Index out of range",
+			args: args{
+				"example-input{3",
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "Fail with String length",
+			args: args{
+				"example-input{three}",
+			},
+
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "Fail with Special Character with Integer length",
+			args: args{
+				"example-input{3!}",
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "Fail with Float length",
+			args: args{
+				"example-input{3.0}",
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseLength(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseLength() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("parseLength() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
