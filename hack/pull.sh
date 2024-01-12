8ab9e850ff505980d7c7844e951433b9389786d8
Volkan Özçelik <ovolkan@vmware.com>
2024-01-11 19:37:46 -0800

    one more script
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ hack/pull.sh
@@ -0,0 +1,19 @@
+/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+export GIT_SSH_COMMAND="ssh -i /home/vsecm/.ssh/id_ed25519"
+
+cd /home/aegis/WORKSPACE/VSecM/ || exit
+
+git stash
+git checkout main
+git pull
