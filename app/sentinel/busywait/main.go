de1db2c9a0fd85d8da64fef158a7670506b2969e
Volkan Özçelik <ovolkan@vmware.com>
2024-02-01 23:10:06 -0800

    🌟 enhancement(VSecM Sentinel): have an init script for vsecm-sentinel
    the init script will autogenerate secrets when the sentinel brings itself up.
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

--- app/sentinel/busywait/main.go
+++ app/sentinel/busywait/main.go
@@ -11,10 +11,124 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"github.com/vmware-tanzu/secrets-manager/app/sentinel/internal/safe"
+	"github.com/vmware-tanzu/secrets-manager/core/env"
+	"github.com/vmware-tanzu/secrets-manager/core/log"
 	"github.com/vmware-tanzu/secrets-manager/core/probe"
 	"github.com/vmware-tanzu/secrets-manager/core/system"
-	"github.com/vmware-tanzu/secrets-manager/core/util"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
+
+const delimiter = "--"
+const separator = ":"
+const sleep = "sleep"
+
+type command string
+
+const workload command = "w"
+const namespace command = "n"
+const secret command = "s"
+const transformation command = "t"
+
+type SentinelCommand struct {
+	Workload       string
+	Namespace      string
+	Secret         string
+	Transformation string
+	UseKubernetes  bool
+	DeleteSecret   bool
+	AppendSecret   bool
+	BackingStore   string
+	Format         string
+	Encrypt        bool
+	NotBefore      string
+	Expires        string
+}
+
+func processCommandBlock(ctx context.Context, sc SentinelCommand) {
+	// TODO: change the signature of the function to accept the context.
+	safe.Post(ctx, sc)
+}
+
+func doSleep(seconds int) {
+	time.Sleep(time.Duration(seconds) * time.Second)
+}
+
+func executeInitCommand() {
+	cid := "VSECMSENTINEL"
+	filePath := env.SentinelInitCommandPath()
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.InfoLn(
+			&cid,
+			"no initialization file found… skipping custom initialization.",
+		)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	ctx := context.Background()
+
+	scanner := bufio.NewScanner(file)
+	var sc SentinelCommand
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" {
+			continue
+		}
+
+		if line == delimiter {
+			processCommandBlock(ctx, sc)
+			sc = SentinelCommand{}
+			continue
+		}
+
+		parts := strings.SplitN(line, separator, 2)
+
+		if len(parts) != 2 {
+			continue
+		}
+
+		if parts[0] == sleep {
+			seconds, _ := strconv.Atoi(parts[1])
+			doSleep(seconds)
+			continue
+		}
+
+		key := parts[0]
+		value := parts[1]
+
+		switch command(key) {
+		case workload:
+			sc.Workload = value
+		case namespace:
+			sc.Namespace = value
+		case secret:
+			sc.Secret = value
+		case transformation:
+			sc.Transformation = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.ErrorLn(
+			&cid,
+			"Error reading initialization file: ",
+			err.Error(),
+		)
+	}
+}
 
 func main() {
 	id := "VSECMSENTINEL"
@@ -22,8 +136,13 @@ func main() {
 	go probe.CreateLiveness()
 
 	//Print the diagnostic information about the environment.
-	envVarsToPrint := []string{"APP_VERSION", "VSECM_LOG_LEVEL", "VSECM_SENTINEL_SECRET_GENERATION_PREFIX"}
-	go util.PrintEnvironmentInfo(&id, envVarsToPrint)
+	envVarsToPrint := []string{"APP_VERSION", "VSECM_LOG_LEVEL",
+		"VSECM_SENTINEL_SECRET_GENERATION_PREFIX"}
+	log.PrintEnvironmentInfo(&id, envVarsToPrint)
+
+	log.InfoLn(&id, "Executing the initialization commands (if any)")
+	executeInitCommand()
+	log.InfoLn(&id, "Initialization commands executed successfully")
 
 	// Run on the main thread to wait forever.
 	system.KeepAlive()
---
+++ examples/vsecm-workshop/hack/014-get-root-key.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets… secret
+# >/
+# <>/' Copyright 2023–present VMware, Inc.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+echo ""
+kubectl get secret vsecm-safe-age-key -n vsecm-system \
+  -o jsonpath="{.data.KEY_TXT}" | base64 --decode
+echo ""
