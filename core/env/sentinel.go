1a46bdd426241b80640add4911941ccfceff3af3
Volkan Özçelik <ovolkan@vmware.com>
2024-02-06 22:57:52 -0800

    📚 docs(VSecM): add godoc to public methods
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

--- core/env/sentinel.go
+++ core/env/sentinel.go
@@ -12,6 +12,16 @@ package env
 
 import "os"
 
+// SentinelInitCommandPath returns the path to the initialization commands file
+// for VSecM Sentinel.
+//
+// It checks for an environment variable "VSECM_SENTINEL_INIT_COMMAND_PATH" and
+// uses its value as the path. If the environment variable is not set, it
+// defaults to "/opt/vsecm-sentinel/init/data".
+//
+// Returns:
+//
+//	string: The path to the Sentinel initialization commands file.
 func SentinelInitCommandPath() string {
 	p := os.Getenv("VSECM_SENTINEL_INIT_COMMAND_PATH")
 	if p == "" {
@@ -20,8 +30,19 @@ func SentinelInitCommandPath() string {
 	return p
 }
 
-// TODO: add docs and also add godocs.
-
+// SentinelInitCommandTombstonePath returns the path for the VSecM Sentinel
+// initialization command tombstone file.
+//
+// It looks for the environment variable "VSECM_SENTINEL_INIT_COMMAND_TOMBSTONE_PATH"
+// and uses its value as the path. If the environment variable is not set, it
+// defaults to "/opt/vsecm-sentinel/tombstone/init".
+//
+// This path is usually used to store a "tombstone" file or data indicating that
+// the initialization command has been executed or is no longer valid.
+//
+// Returns:
+//
+//	string: The path to the Sentinel initialization command tombstone.
 func SentinelInitCommandTombstonePath() string {
 	p := os.Getenv("VSECM_SENTINEL_INIT_COMMAND_TOMBSTONE_PATH")
 	if p == "" {
