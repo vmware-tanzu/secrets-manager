150c6f64fe88417e5bf007e6618ce53836573dd2
Volkan Özçelik <ovolkan@vmware.com>
2024-03-06 18:55:22 -0800

    chart update
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets... secret
+# >/
+# <>/' Copyright 2023-present VMware Secrets Manager contributors.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+helm package "helm-charts/0.23.1-tcx" --version="0.23.1-tcx"
