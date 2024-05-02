747788f8edbd087d50d88b5b0f21e456cb3c474b
Volkan Özçelik <ovolkan@vmware.com>
2024-05-02 18:16:16 -0400

    0.25.2 - intermediate release cut (#953)
    Signed-off-by: Volkan Özçelik <volkan.ozcelik@broadcom.com>

--- examples/operator_decrpyt_secrets/reveal.sh
+++ examples/operator_decrpyt_secrets/reveal.sh
@@ -9,7 +9,7 @@
 # <>/' Copyright 2023-present VMware Secrets Manager contributors.
 # >/'  SPDX-License-Identifier: BSD-2-Clause
 # */
-VERSION="0.25.1"
+VERSION="0.25.2"
 
 docker run --rm \
   -v "$(pwd)":/vsecm \
