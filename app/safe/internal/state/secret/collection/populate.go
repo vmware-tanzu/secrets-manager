98bfa88d33b0319676c15dd809fac6b58da7feeb
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-05-16 15:03:20 -0700

    secret population logic
    Signed-off-by: Volkan Özçelik <volkan.ozcelik@broadcom.com>

--- app/safe/internal/state/secret/collection/populate.go
+++ app/safe/internal/state/secret/collection/populate.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/vmware-tanzu/secrets-manager/app/safe/internal/state/io"
 	"github.com/vmware-tanzu/secrets-manager/app/safe/internal/state/stats"
+	"github.com/vmware-tanzu/secrets-manager/core/entity/v1/data"
 	"github.com/vmware-tanzu/secrets-manager/core/env"
 	log "github.com/vmware-tanzu/secrets-manager/core/log/std"
 )
@@ -38,32 +39,12 @@ func SecretsPopulated() bool {
 	return secretsPopulated
 }
 
-// PopulateSecrets scans the designated secrets storage directory on disk, reading each secret file
-// that is not marked as a backup, and loads the secrets into a global store if they have not already
-// been loaded. This ensures that the application's current session has access to all persisted secrets.
-// It uses a locking mechanism to prevent concurrent execution and ensure data consistency.
-//
-// Parameters:
-//   - cid (string): A correlation ID that is used for logging purposes, allowing for the tracing of
-//     the populate operation through logs.
-//
-// Returns:
-//   - error: If an error occurs during the directory reading or secret reading process, it returns
-//     an error wrapped with context about the failure point. If no errors occur, it returns nil to
-//     indicate successful completion.
-func PopulateSecrets(cid string) error {
-	log.TraceLn(&cid, "populateSecrets: populating secrets...")
-	secretsPopulatedLock.Lock()
-	defer secretsPopulatedLock.Unlock()
-
-	// TODO: switch based on persistence mode (configurable from env)
-	// if mode file; get to disc
-	// any other mode shall panic for now
-
+func populateSecretsFromFileStore(cid string) error {
 	root := env.DataPathForSafe()
 	files, err := os.ReadDir(root)
 	if err != nil {
-		return errors.Wrap(err, "populateSecrets: problem reading secrets directory")
+		return errors.Wrap(err,
+			"populateSecrets: problem reading secrets directory")
 	}
 
 	for _, file := range files {
@@ -85,7 +66,9 @@ func PopulateSecrets(cid string) error {
 
 		secretOnDisk, err := io.ReadFromDisk(key)
 		if err != nil {
-			log.ErrorLn(&cid, "populateSecrets: problem reading secret from disk:", err.Error())
+			log.ErrorLn(&cid,
+				"populateSecrets: problem reading secret from disk:",
+				err.Error())
 			continue
 		}
 		if secretOnDisk != nil {
@@ -94,7 +77,60 @@ func PopulateSecrets(cid string) error {
 		}
 	}
 
-	secretsPopulated = true
+	return nil
+}
+
+// PopulateSecrets scans the designated secrets storage directory on disk,
+// reading each secret file that is not marked as a backup, and loads the
+// secrets into a global store if they have not already been loaded. This
+// ensures that the application's current session has access to all persisted
+// secrets. It uses a locking mechanism to prevent concurrent execution and
+// ensure data consistency.
+//
+// Parameters:
+//   - cid (string): A correlation ID that is used for logging purposes,
+//     allowing for the tracing of the populate operation through logs.
+//
+// Returns:
+//   - error: If an error occurs during the directory reading or secret reading
+//     process, it returns an error wrapped with context about the failure
+//     point. If no errors occur, it returns nil to indicate successful
+//     completion.
+func PopulateSecrets(cid string) error {
+	log.TraceLn(&cid, "populateSecrets: populating secrets...")
+
+	secretsPopulatedLock.Lock()
+	defer secretsPopulatedLock.Unlock()
+
+	// Already populated, nothing to do.
+	if secretsPopulated {
+		return nil
+	}
+
+	store := env.BackingStoreForSafe()
+
+	populated := false
+	switch store {
+	case data.Memory:
+		log.TraceLn(&cid, "populateSecrets: using in-memory store.")
+	case data.File:
+		err := populateSecretsFromFileStore(cid)
+		populated = err == nil
+		if err != nil {
+			log.ErrorLn(&cid, "populateSecrets:error", err.Error())
+		}
+	case data.Kubernetes:
+		panic("implement kubernetes store")
+	case data.AwsSecretStore:
+		panic("implement aws secret store")
+	case data.AzureSecretStore:
+		panic("implement azure secret store")
+	case data.GcpSecretStore:
+		panic("implement gcp secret store")
+	}
+	secretsPopulated = populated
+
 	log.TraceLn(&cid, "populateSecrets: secrets populated.")
+
 	return nil
 }
