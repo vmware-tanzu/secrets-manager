1248eae801e8dc70b744af7cbf1438b614db2fba
Volkan Özçelik <ovolkan@vmware.com>
2024-05-31 11:53:22 -0700

    minor refactoring (#973)
    * minor refactoring
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    * minor refactoring
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>
    
    ---------
    
    Signed-off-by: Volkan Özçelik <ovolkan@vmware.com>

---
+++ core/audit/journal/print.go
@@ -0,0 +1,29 @@
+/*
+|    Protect your secrets, protect your sensitive data.
+:    Explore VMware Secrets Manager docs at https://vsecm.com/
+</
+<>/  keep your secrets... secret
+>/
+<>/' Copyright 2023-present VMware Secrets Manager contributors.
+>/'  SPDX-License-Identifier: BSD-2-Clause
+*/
+
+package journal
+
+import (
+	"github.com/vmware-tanzu/secrets-manager/core/audit/state"
+	"github.com/vmware-tanzu/secrets-manager/core/log/std"
+)
+
+func printAudit(correlationId string, e state.Event,
+	method, url, spiffeid, payload string) {
+	std.AuditLn(
+		&correlationId,
+		string(e),
+		"{{"+
+			"method:[["+method+"]],"+
+			"url:[["+url+"]],"+
+			"spiffeid:[["+spiffeid+"]],"+
+			"payload:[["+payload+"]]}}",
+	)
+}
