b34d1444d448a20db30d0fa2a9cb30ae3ce5888c
Omer Kocaoglu <omergk28@gmail.com>
2024-07-14 15:15:16 -0400

    🚨 test(VSecM): 902 add test for url.go
    Closes: 902
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>

--- core/log/rpc/url_test.go
+++ core/log/rpc/url_test.go
@@ -9,3 +9,26 @@
 */
 
 package rpc
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSentinelLoggerUrl(t *testing.T) {
+	tests := []struct {
+		envValue string
+		expected string
+	}{
+		{"", "localhost:50051"},
+		{"testLoggerUrl", "testLoggerUrl"},
+	}
+
+	for _, test := range tests {
+		_ = os.Setenv("VSECM_SENTINEL_LOGGER_URL", test.envValue)
+		actual := SentinelLoggerUrl()
+		if actual != test.expected {
+			t.Errorf("SentinelLoggerUrl() with env value %q = %v; expected %v", test.envValue, actual, test.expected)
+		}
+	}
+}
