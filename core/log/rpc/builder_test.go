60833bd774aa4c93c5207189f506d00d0df31227
omergk28 <omergk28@gmail.com>
2024-07-20 12:58:02 -0400

    899 builder test (#1074)
    * 🚨 test(VSecM): 899 add tests for core/log/rpc/builder.go
    
    Closes: 899
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>
    
    * 🚨 test(VSecM): 899 follow got/want pattern
    
    Closes: 899
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>
    
    * 🚨 test(VSecM): 899 fix typo
    
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>
    
    ---------
    
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>

--- core/log/rpc/builder_test.go
+++ core/log/rpc/builder_test.go
@@ -9,3 +9,64 @@
 */
 
 package rpc
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestBuild(t *testing.T) {
+
+	testCorrelationID := "23B3BB0E-DB5D-4F90-B980-F0215232A017"
+	dateTimeRegex := `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}`
+
+	tests := []struct {
+		name          string
+		logHeader     string
+		correlationId *string
+		wantRegex     string
+		messageParts  []any
+	}{
+		{
+			name:          "Valid values",
+			logHeader:     "LOG",
+			correlationId: &testCorrelationID,
+			messageParts:  []any{"Test", "message"},
+			wantRegex:     `^LOG\[` + dateTimeRegex + `\] 23B3BB0E-DB5D-4F90-B980-F0215232A017 Test message\n$`,
+		},
+		{
+			name:          "Empty log header / mixed type message parts",
+			logHeader:     "",
+			correlationId: &testCorrelationID,
+			messageParts:  []any{"Test", nil, "message", 3},
+			wantRegex:     `^\[` + dateTimeRegex + `\] 23B3BB0E-DB5D-4F90-B980-F0215232A017 Test <nil> message 3\n$`,
+		},
+		{
+			name:          "Nil correlationId",
+			logHeader:     "LOG",
+			correlationId: nil,
+			messageParts:  []any{"Test", "message", 4},
+			wantRegex:     `^LOG\[` + dateTimeRegex + `\] Test message 4\n$`,
+		},
+		{
+			name:          "Nil message",
+			logHeader:     "LOG",
+			correlationId: &testCorrelationID,
+			messageParts:  nil,
+			wantRegex:     `^LOG\[` + dateTimeRegex + `\] 23B3BB0E-DB5D-4F90-B980-F0215232A017\n$`,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := build(tc.logHeader, tc.correlationId, tc.messageParts[:]...)
+			matched, err := regexp.MatchString(tc.wantRegex, got)
+			if err != nil {
+				t.Fatalf("Regex match failed: %v", err)
+			}
+			if !matched {
+				t.Errorf("build() = %q, want match regex %q", got, tc.wantRegex)
+			}
+		})
+	}
+}
