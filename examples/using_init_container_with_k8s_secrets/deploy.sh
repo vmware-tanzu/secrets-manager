7bf9fa85c2e6a87c35eb4411f4b29084b614c0c8
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-08-07 22:33:48 -0700

    fix
    Signed-off-by: Volkan Özçelik <volkan.ozcelik@broadcom.com>

---
+++ examples/using_init_container_with_k8s_secrets/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets... secret
+# >/
+# <>/' Copyright 2023-present VMware Secrets Manager contributors.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+kubectl apply -f ./k8s/ServiceAccount.yaml
+kubectl apply -f ./k8s/Deployment.yaml
+kubectl apply -f ./k8s/Identity.yaml
