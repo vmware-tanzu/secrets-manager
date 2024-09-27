2e04c9a200c4ba3450c3175d25b20bd88e008751
Volkan Özçelik <ovolkan@vmware.com>
2024-09-26 20:08:02 -0700

    add helper scripts to workshop (#1141)
    Signed-off-by: Volkan Özçelik <volkan.ozcelik@broadcom.com>

---
+++ examples/workshop_spiffe_federation/clusters/azmodan/hack/federate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# /*
+# |    Protect your secrets, protect your sensitive data.
+# :    Explore VMware Secrets Manager docs at https://vsecm.com/
+# </
+# <>/  keep your secrets... secret
+# >/
+# <>/' Copyright 2023-present VMware Secrets Manager contributors.
+# >/'  SPDX-License-Identifier: BSD-2-Clause
+# */
+
+go run federate.go
+./set-initial-bundle.sh
+echo "Federation established."
