df120c323623b1264c7f52303fa8c29b99435e49
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-10-01 14:02:10 -0700

    version bump to 0.27.3

--- helm-charts-playground/app/main.go
+++ helm-charts-playground/app/main.go
@@ -117,12 +117,12 @@ func copyVSecMCrds(inputDir, outputDir string) {
 }
 
 func main() {
-	inputFile := "/Users/volkan/Desktop/WORKSPACE/secrets-manager/k8s/0.27.2/spire.yaml"
+	inputFile := "/Users/volkan/Desktop/WORKSPACE/secrets-manager/k8s/0.27.3/spire.yaml"
 	outputDir := "/Users/volkan/Desktop/WORKSPACE/secrets-manager/helm-charts-playground/vsecm-manifests"
 
 	createManifests(inputFile, outputDir)
 
-	//inputDir := "/Users/volkan/Desktop/WORKSPACE/secrets-manager/k8s/0.27.2/crds"
+	//inputDir := "/Users/volkan/Desktop/WORKSPACE/secrets-manager/k8s/0.27.3/crds"
 	//outputDir = "/Users/volkan/Desktop/WORKSPACE/secrets-manager/helm-charts-playground/vsecm-manifests/crds"
 	//
 	//copyVSecMCrds(inputDir, outputDir)
