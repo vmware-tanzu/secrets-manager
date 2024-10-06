53c52cc0cdc56c8444c1d9554908b67a0b52d305
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-10-06 01:26:15 -0700

    fix encryption

--- examples/workshop_spiffe_federation/apps/control-plane-server/server.go
+++ examples/workshop_spiffe_federation/apps/control-plane-server/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
@@ -43,8 +45,9 @@ type Secret struct {
 }
 
 type EncryptedResponse struct {
-	EncryptedData string `json:"encryptedData"`
-	Signature     string `json:"signature"`
+	EncryptedAESKey string `json:"encryptedAESKey"`
+	EncryptedData   string `json:"encryptedData"`
+	Signature       string `json:"signature"`
 }
 
 type Secrets struct {
@@ -129,6 +132,31 @@ func parsePublicKey(publicKeyPEM string) (*rsa.PublicKey, error) {
 	}
 
 	return rsaPub, nil
+}
+
+func generateAESKey() ([]byte, error) {
+	key := make([]byte, 32) // AES-256
+	_, err := rand.Read(key)
+	return key, err
+}
+
+func encryptAES(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
 //func generateKeyPair() error {
@@ -229,7 +257,6 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Generate a new keypair for this response
 	privateKey, publicKey, err := generateKeyPair()
 	if err != nil {
-		fmt.Println("error generating keypair")
 		http.Error(w, "Error generating keypair", http.StatusInternalServerError)
 		return
 	}
@@ -245,32 +272,48 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if secretValue == nil {
-		fmt.Println("No secret found for the given SPIFFE ID")
 		http.Error(w, "No secret found for the given SPIFFE ID", http.StatusNotFound)
 		return
 	}
 
-	// Encrypt the secret using the client's public key
-	encryptedData, err := encryptData(secretValue, clientPublicKey)
+	// Generate AES key
+	aesKey, err := generateAESKey()
 	if err != nil {
-		fmt.Println("error encrypting data")
-		fmt.Println(err.Error())
+		http.Error(w, "Error generating AES key", http.StatusInternalServerError)
+		return
+	}
+
+	// Encrypt the AES key with the client's public key
+	encryptedAESKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, clientPublicKey, aesKey, nil)
+	if err != nil {
+		http.Error(w, "Error encrypting AES key", http.StatusInternalServerError)
+		return
+	}
+
+	// Encrypt the secret with AES
+	plaintext, err := json.Marshal(secretValue)
+	if err != nil {
+		http.Error(w, "Error marshaling secret", http.StatusInternalServerError)
+		return
+	}
+	encryptedData, err := encryptAES(plaintext, aesKey)
+	if err != nil {
 		http.Error(w, "Error encrypting data", http.StatusInternalServerError)
 		return
 	}
 
-	// Sign the encrypted data using the newly generated private key
+	// Sign the encrypted data
 	signature, err := signData(encryptedData, privateKey)
 	if err != nil {
-		fmt.Println("error signing data")
 		http.Error(w, "Error signing data", http.StatusInternalServerError)
 		return
 	}
 
 	// Prepare the response
 	response := EncryptedResponse{
-		EncryptedData: base64.StdEncoding.EncodeToString(encryptedData),
-		Signature:     base64.StdEncoding.EncodeToString(signature),
+		EncryptedAESKey: base64.StdEncoding.EncodeToString(encryptedAESKey),
+		EncryptedData:   base64.StdEncoding.EncodeToString(encryptedData),
+		Signature:       base64.StdEncoding.EncodeToString(signature),
 	}
 
 	// Encode the public key to PEM format
@@ -285,6 +328,66 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Send the response
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(response)
+
+	//// Generate a new keypair for this response
+	//privateKey, publicKey, err := generateKeyPair()
+	//if err != nil {
+	//	fmt.Println("error generating keypair")
+	//	http.Error(w, "Error generating keypair", http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//// Find the corresponding secret
+	//secretName := fmt.Sprintf("vsecm-relay:%s", trustDomain)
+	//var secretValue []string
+	//for _, secret := range secrets.Secrets {
+	//	if secret.Name == secretName {
+	//		secretValue = secret.Value
+	//		break
+	//	}
+	//}
+	//
+	//if secretValue == nil {
+	//	fmt.Println("No secret found for the given SPIFFE ID")
+	//	http.Error(w, "No secret found for the given SPIFFE ID", http.StatusNotFound)
+	//	return
+	//}
+	//
+	//// Encrypt the secret using the client's public key
+	//encryptedData, err := encryptData(secretValue, clientPublicKey)
+	//if err != nil {
+	//	fmt.Println("error encrypting data")
+	//	fmt.Println(err.Error())
+	//	http.Error(w, "Error encrypting data", http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//// Sign the encrypted data using the newly generated private key
+	//signature, err := signData(encryptedData, privateKey)
+	//if err != nil {
+	//	fmt.Println("error signing data")
+	//	http.Error(w, "Error signing data", http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//// Prepare the response
+	//response := EncryptedResponse{
+	//	EncryptedData: base64.StdEncoding.EncodeToString(encryptedData),
+	//	Signature:     base64.StdEncoding.EncodeToString(signature),
+	//}
+	//
+	//// Encode the public key to PEM format
+	//publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+	//	Type:  "RSA PUBLIC KEY",
+	//	Bytes: x509.MarshalPKCS1PublicKey(publicKey),
+	//})
+	//
+	//// Add the public key to the response headers
+	//w.Header().Set("X-Public-Key", base64.StdEncoding.EncodeToString(publicKeyPEM))
+	//
+	//// Send the response
+	//w.Header().Set("Content-Type", "application/json")
+	//_ = json.NewEncoder(w).Encode(response)
 }
 
 func main() {
