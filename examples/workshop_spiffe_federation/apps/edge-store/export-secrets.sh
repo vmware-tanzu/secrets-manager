9b229d1d8e50694f8195d624f25bee9c2f57ff68
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-10-05 19:30:07 -0700

    export secrets

--- examples/workshop_spiffe_federation/apps/edge-store/export-secrets.sh
+++ examples/workshop_spiffe_federation/apps/edge-store/export-secrets.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Find the pod
 S=$(microk8s kubectl get po -n vsecm-system \
   | grep "vsecm-sentinel-" | awk '{print $1}')
-export S=$S
 
-microk8s kubectl exec "$S" -n vsecm-system -- safe -l -e | sed -n '/^{/,/^}/p'
+# Execute the command and save output directly to secrets.json in the project root
+microk8s kubectl exec "$S" -n vsecm-system -- safe -l -e > ./secrets.json
+
+# Inform the user
+echo "Secrets have been saved to secrets.json in the current working directory."
