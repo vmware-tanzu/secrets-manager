d6c4107acdb6596be06c3e7a7cd3622f4b12856f
Volkan Özçelik <volkan.ozcelik@broadcom.com>
2024-10-19 16:50:07 -0700

    recent changes

--- examples/workshop_vsecm_eso/hack/create-certs.sh
+++ examples/workshop_vsecm_eso/hack/create-certs.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+SCOUT_DNS="vsecm-scout.vsecm-system.svc.cluster.local"
+
 # Generate a private key
 openssl genrsa -out server.key 2048
 
 # Create a self-signed certificate
 openssl req -new -x509 -sha256 -key server.key -out server.crt \
-  -days 3650 -subj "/CN=vsecm-scout.default.svc.cluster.local"
+  -days 3650 -subj "/CN=${SCOUT_DNS}"
 
 # Create a CA bundle (in this case, it's just the server certificate)
 cp server.crt ca.crt
@@ -15,6 +17,8 @@ cp server.crt ca.crt
 CA_BUNDLE=$(cat ca.crt | base64 -w 0)
 
 # Update the cluster-secret-store.yaml file
+# There is no sensitive info in it, so it can
+# be sent to a remote cluster via GitOps.
 cat << EOF > cluster-secret-store.yaml
 apiVersion: external-secrets.io/v1beta1
 kind: ClusterSecretStore
@@ -23,16 +27,17 @@ metadata:
 spec:
   provider:
     webhook:
-      url: "https://vsecm-scout.default.svc.cluster.local:8443/webhook?key={{ .remoteRef.key }}"
+      url: "https://${SCOUT_DNS}/webhook?key={{ .remoteRef.key }}"
       method: GET
       result:
         jsonPath: "$"
       caBundle: ${CA_BUNDLE}
 EOF
 
-echo "Self-signed certificate generated and ClusterSecretStore YAML updated."
+echo "Certificates generated and ClusterSecretStore YAML updated."
 echo "Please review the cluster-secret-store.yaml file."
 
+# These will be stored in VSecM Safe and dynamically provided to VSecM Scout.
 mv server.crt ./hack
 mv server.key ./hack
 mv ca.crt ./hack
--- examples/workshop_vsecm_eso/hack/update-vsecm.sh
+++ examples/workshop_vsecm_eso/hack/update-vsecm.sh
@@ -20,24 +20,24 @@ S=$(kubectl get po -n vsecm-system \
 export S=$S
 
 # Add cert and key to vsecm
-kubectl exec -n vsecm-system $S -- safe \
+kubectl exec -n vsecm-system "$S" -- safe \
   -w "raw:vsecm-scout-crt" \
 	-s "$SERVER_CRT"
 
-kubectl exec -n vsecm-system $S -- safe \
+kubectl exec -n vsecm-system "$S" -- safe \
   -w "raw:vsecm-scout-key" \
 	-s "$SERVER_KEY"
 
-kubectl exec -n vsecm-system $S -- safe \
+kubectl exec -n vsecm-system "$S" -- safe \
   -w "raw:vsecm-scout-ca-crt" \
 	-s "$CA_CRT"
 
 # Add JWT secret
-kubectl exec -n vsecm-system $S -- safe \
+kubectl exec -n vsecm-system "$S" -- safe \
   -w "raw:vsecm-scout-jwt-secret" \
   -s "$JWT_SECRET"
 
 # This is the demo secret:
-kubectl exec -n vsecm-system $S -- safe \
+kubectl exec -n vsecm-system "$S" -- safe \
   -w "raw:coca-cola.cluster-001" \
   -s '{"namespaces": {"cokeSystem": {"secrets":{"adminCredentials":{"type":"k8s","value":"super-secret-secret","metadata": {"labels": {"managedBy": "coke-system"},"annotations": {"injectSidecar": "true"},"creationTimestamp": "2024-01-01","lastUpdated": "2024-01-01"},"expires": "2024-01-01","notBefore": "2024-01-01"}}}}}'
